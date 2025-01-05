a5eedc7e5f7efa99917e10a356c6c8ccadddde4f
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-04 20:15:33 -0600

    Testing that the __repr__ works as expected

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_print_repr(self):
+        node = TextNode("This is a test node", TextType.ITALIC)
+        printed = node.__repr__()
+        expected = "TextNode(This is a test node, TextType.ITALIC, None)"
+        self.assertEqual(printed, expected)
 
 if __name__ == "__main__":
     unittest.main()
